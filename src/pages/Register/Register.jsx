6a29a8474145dc11e9be3beb42ef3cc0d026a9fb
Azim <azimuddin807@gmail.com>
2024-12-19 11:49:14 +0600

    secure firebase config

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,6 +3,25 @@ import Lottie from "lottie-react";
 import LottieRegiterData from './../../assets/loitte/register.json'
 
 const Register = () => {
+
+  const handleRegister = e => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    // Email validation: At least 6 characters, one uppercase, one lowercase, and one number
+    const emailPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/;
+
+    if (!emailPattern.test(email)) {
+        console.error("Email must be at least 6 characters, include one uppercase letter, one lowercase letter, and one number.");
+        return;
+    }
+
+    console.log(email, password);
+};
+
+
     return (
         <div className="hero  min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
@@ -14,19 +33,20 @@ const Register = () => {
 
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <h1 className="text-5xl font-bold p-5 text-center">Login now!</h1>
-            <form className="card-body">
+          <h1 className="text-5xl font-bold p-5 text-center">Register now!</h1>
+
+            <form onSubmit={handleRegister} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="email" placeholder="email" className="input input-bordered" required />
+                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="password" placeholder="password" className="input input-bordered" required />
+                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                 </label>
